dda0557b05cb0406194872130de41d3925f85a00
Lobaugh, Christian <christian.lobaugh@hireright.com>
2020-01-28 18:16:34 -0600

    change syntax highlighting style

--- web/src/components/Code.js
+++ web/src/components/Code.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import SyntaxHighlighter from 'react-syntax-highlighter';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark} from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 export default ({node}) => {
   if (!node || !node.code) { return null }
   const {language, code} = node 
   return (
-    <SyntaxHighlighter language={language || 'text'}>
+    <SyntaxHighlighter language={language || 'text'} style={atomDark}>
       {code}
     </SyntaxHighlighter>
   )
