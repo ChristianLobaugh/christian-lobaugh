9d99ba8f9a2b1e8f420ad8400bfe701723d9fb56
Christian Lobaugh <christian.lobaugh@gmail.com>
2020-05-28 00:21:19 -0500

    fixed spacing for linter

--- web/src/components/Code.js
+++ web/src/components/Code.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { atomDark} from 'react-syntax-highlighter/dist/esm/styles/prism';
+import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter'
+import {atomDark} from 'react-syntax-highlighter/dist/esm/styles/prism'
 
 export default ({node}) => {
   if (!node || !node.code) { return null }
-  const {language, code} = node 
+  const {language, code} = node
   return (
     <SyntaxHighlighter language={language || 'text'} style={atomDark}>
       {code}
